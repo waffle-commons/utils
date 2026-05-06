c3f97d1a9c79ab60d02d0156130a6765db4313be
Leslie Petrimaux <leslypetrimaux@gmail.com>
2026-05-06 15:55:22 +0200

    update `mago` configuration

--- tests/src/AbstractTestCase.php
+++ tests/src/AbstractTestCase.php
@@ -8,15 +8,4 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class AbstractTestCase extends BaseTestCase
 {
-    #[\Override]
-    protected function setUp(): void
-    {
-        parent::setUp();
-    }
-
-    #[\Override]
-    protected function tearDown(): void
-    {
-        parent::tearDown();
-    }
 }
